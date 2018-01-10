990577a6b4784fcdef9997722eb034093e0ec6e1
ai-coder <akashkumar0216@gmail.com>
2018-01-10 18:21:22 +0530

    work_try

--- articulate.js
+++ articulate.js
@@ -1,12 +1,11 @@
 window.onload = init;
 
 function init(){
-	var string_en = document.getElementById('topics');
-
+	var string_en = document.form-arti.topics.value;
 
 }
 
 function getFiles(){
-	var string_en = document.getElementById('topics');
+	var string_en = document.form-arti.topics.value;
 	alert(string_en)
 }
